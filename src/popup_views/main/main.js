c8f95dce9acac2b6831ec69a75829d563b365ad4
Votre Nom <ngounot@et.esiea.fr>
2023-02-28 22:34:47 +0100

    essai non fonctionnel (mauvais format de donné)

--- src/popup_views/main/main.js
+++ src/popup_views/main/main.js
@@ -11,30 +11,32 @@ import {
 } from 'firebase/firestore'
 
 // ========== VARIABLES ========
-var timecode = 0
-var userid
-let comment = ''
-let mess = ''
-let limite = 10 //limite de message du chat
-let numMess = 0 //nombre de messages affichés depuis le début
-let messageElement = '' //élément injecter pour afficher le commentaire
-let preced = '' //div du message precedent
+var timecode = 0;
+var userid;
+let comment = '';
+let currentCommentsList;
+let messages;
+let limite = 10 ;//limite de message du chat
+let numMess = 0 ;//nombre de messages affichés depuis le début
+// let messageElement = ''; //élément injecter pour afficher le commentaire
+let preced = ''; //div du message precedent
 
-let mainPort = chrome.runtime.connect({ name: 'main_status' })
+
+let mainPort = chrome.runtime.connect({ name: 'main_status' });
+var currentEpisode;
 
 let ordersForBg = chrome.runtime.connect({
   name: 'orders_from_main'
 })
 
-let currentUser = null
+let currentUser = null;
 
-let previousCommentsList = []
-let currentCommentsList = []
 
-let previousEpisodeTitle = ""
-let currentEpisodeTitle = ""
 
-let foregroundTabId = null
+let previousEpisodeTitle = "";
+let currentEpisodeTitle = "";
+
+let foregroundTabId = null;
 
 let commentObjectToSend = {
   podcastEpisode: {},
@@ -42,7 +44,7 @@ let commentObjectToSend = {
   userName: '',
   userId: '',
   comment: '',
-  private: false
+  private: false 
 }
 
 let commentToSend = {
@@ -67,49 +69,49 @@ let currentData = {
   }
 }
 
-let ordersForFg = null
+let ordersForFg = null;
 
 
 
 // ============ CODE ===========
 document.addEventListener('DOMContentLoaded', function () {
 
-  currentUser = getCurrentUser()
-  checkUserLogin()
+  currentUser = getCurrentUser();
+  checkUserLogin();
 
   chrome.storage.local.get(['foregroundTabId'], function (data) {
-    console.log('foregroundTabId : ', data.foregroundTabId)
+    console.log('foregroundTabId : ', data.foregroundTabId);
     if (data.foregroundTabId != null) {
-      ordersForFg = chrome.tabs.connect(data.foregroundTabId, {name: 'orders_from_main'})
+      ordersForFg = chrome.tabs.connect(data.foregroundTabId, {name: 'orders_from_main'});
 
       document.querySelector('#minus_ten').addEventListener('click', () => {
-        ordersForFg.postMessage({ order: 'click_minus_ten' })
+        ordersForFg.postMessage({ order: 'click_minus_ten' });
       })
 
       document.querySelector('#play_pause').addEventListener('click', () => {
-        switchPlayPauseButton()
-        ordersForFg.postMessage({ order: 'click_play_pause' })
+        switchPlayPauseButton();
+        ordersForFg.postMessage({ order: 'click_play_pause' });
       })
 
       document.querySelector('#plus_thirty').addEventListener('click', () => {
-        ordersForFg.postMessage({ order: 'click_plus_thirty' })
+        ordersForFg.postMessage({ order: 'click_plus_thirty' });
       })
     }
   })
 
   document.querySelector('#btn_user_profile').addEventListener('click', () => {
-    window.location.replace('./settings.html')
+    window.location.replace('./settings.html');
   })
 
   document.querySelector('#publish_btn').addEventListener('click', () => {
-    let textArea = document.getElementById('text_field')
-    let userComment = textArea.value
-    let toggleStatus = document.getElementById('togBtn').checked
+    let textArea = document.getElementById('text_field');
+    let userComment = textArea.value;
+    let toggleStatus = document.getElementById('togBtn').checked;
 
     if (userComment.length !== 0) {
-      textArea.placeholder = ' votre commentaire'
-      textArea.style.setProperty('color', 'black')
-      textArea.style.setProperty('background-color', 'white')
+      textArea.placeholder = ' votre commentaire';
+      textArea.style.setProperty('color', 'black');
+      textArea.style.setProperty('background-color', 'white');
 
       commentObjectToSend = {
         podcastEpisode: currentData.episode,
@@ -119,55 +121,64 @@ document.addEventListener('DOMContentLoaded', function () {
         comment: userComment,
         private: toggleStatus
       }
-      commentObjectToSend.comment = cleanBadWords(commentObjectToSend.comment)
+      commentObjectToSend.comment = cleanBadWords(commentObjectToSend.comment);
 
-      console.log('currentData.episode.title : ', currentData.episode.title)
-      console.log('commentObjectToSend : ', commentObjectToSend)
-      addDocFirestore(currentData.episode.title, commentObjectToSend)
+      console.log('currentData.episode.title : ', currentData.episode.title);
+      console.log('commentObjectToSend : ', commentObjectToSend);
+      addDocFirestore(currentData.episode.title, commentObjectToSend);
 
       textArea.value = ''
     } else {
-      updateStyleForEmptyComment(textArea)
+      updateStyleForEmptyComment(textArea);
     }
   })
 })
 
-let firstOnSnapshotLaunched = false
-
+let firstOnSnapshotLaunched = false;
+// 
 mainPort.onMessage.addListener(function (msg) {
-  let currentCommentsList
-
+ 
+  let currentOnSnapshot;
+  currentCommentsList = [];
+ 
   if (!firstOnSnapshotLaunched) {
     currentOnSnapshot = onSnapshot(collection(db, msg.title), (snapshot) => {
+      firstOnSnapshotLaunched = true;
       if (!snapshot.empty) {
         snapshot.forEach(doc => {
-          currentCommentsList.push(doc.data())
+          // console.log(doc.data())
+          currentCommentsList.push(doc);
+          getComments(currentCommentsList)
         })
       }
     })
-    firstOnSnapshotLaunched = true
+    firstOnSnapshotLaunched = true;
   }
+  messages = currentCommentsList;
 
-  currentData.userIsLoggedIn = msg.userIsLoggedIn
-  currentData.podcastIsPlaying = msg.podcastIsPlaying
-  currentData.timecode.startingTime = msg.startingTime
-  currentData.timecode.endingTime = msg.endingTime
-  currentData.episode.title = msg.title
-  currentData.episode.rssUrl = msg.rssUrl
+  currentData.userIsLoggedIn = msg.userIsLoggedIn;
+  currentData.podcastIsPlaying = msg.podcastIsPlaying;
+  currentData.timecode.startingTime = msg.startingTime;
+  currentData.timecode.endingTime = msg.endingTime;
+  currentData.episode.title = msg.title;
+  currentData.episode.rssUrl = msg.rssUrl;
+  currentEpisode = currentData.episode;
+  
 
   if (currentEpisodeTitle !== msg.title) {
-    firstOnSnapshotLaunched = false
+    firstOnSnapshotLaunched = false;
 
     currentOnSnapshot = onSnapshot(collection(db, currentEpisode.title), (snapshot) => {
-      console.log("onSnapshot launched")
+      console.log("onSnapshot launched");
       if (!snapshot.empty) {
         snapshot.forEach(doc => {
-          currentCommentsList.push(doc.data())
+          
+          currentCommentsList.push(doc.data());
         })
-        currentData.comments = currentCommentsList
-        console.log('Current comments list : ', currentEpisode.comments)
+        currentData.comments = currentCommentsList;
+        console.log('Current comments list : ', currentEpisode.comments);
       } else {
-        console.log('Collection empty for : ' + currentEpisode.title)
+        console.log('Collection empty for : ' + currentEpisode.title);
       }
     })
   }
@@ -176,45 +187,45 @@ mainPort.onMessage.addListener(function (msg) {
 
 
   if (currentEpisode.title !== '') {
-    let commentsList = []
+    let commentsList = [];
 
     if (currentOnSnapshot !== null) {
       currentOnSnapshot()
-      currentOnSnapshot = null
+      currentOnSnapshot = null;
     }
 
     // TODO : uncomment this code when firestore rules pblm fixed
-    if (currentUser !== null) {
-      currentOnSnapshot = onSnapshot(collection(db, currentEpisode.title), (snapshot) => {
-        console.log("onSnapshot launched")
-        if (!snapshot.empty) {
-          snapshot.forEach(doc => {
-            commentsList.push(doc.data())
-          })
-          currentEpisode.comments = commentsList
-          console.log('Current comments list : ', currentEpisode.comments)
-        } else {
-          console.log('Collection empty for : ' + currentEpisode.title)
-        }
-      })
-    }
+    // if (currentUser !== null) {
+    //   currentOnSnapshot = onSnapshot(collection(db, currentEpisode.title), (snapshot) => {
+    //     console.log("onSnapshot launched")
+    //     if (!snapshot.empty) {
+    //       snapshot.forEach(doc => {
+    //         commentsList.push(doc.data())
+    //       })
+    //       currentEpisode.comments = commentsList
+    //       console.log('Current comments list : ', currentEpisode.comments)
+    //     } else {
+    //       console.log('Collection empty for : ' + currentEpisode.title)
+    //     }
+    //   })
+    // }
   }
 
-  updatePopupPlayer(msg.title, msg.startingTime, msg.endingTime)
-  updateLecture(msg.isPlaying)
+  updatePopupPlayer(msg.title, msg.startingTime, msg.endingTime);
+  updateLecture(msg.isPlaying);
 })
 
 chrome.runtime.onConnect.addListener(function (port) {
   if (port.name === 'comments_from_bg') {
     port.onMessage.addListener(function (msg) {
-      console.log('main.js - Message received from ' + port.name + ' : ', msg)
-      previousCommentsList = currentCommentsList
-      currentCommentsList = msg.comments
+      console.log('main.js - Message received from ' + port.name + ' : ', msg);
+      previousCommentsList = currentCommentsList;
+      // currentCommentsList = msg.comments;
       // TODO : Mettre à jour la popup avec une fonction qui va afficher les nouveaux commentaires
     })
 
     port.onDisconnect.addListener(function () {
-      console.log("main.js - background.js n'est plus actif.")
+      console.log("main.js - background.js n'est plus actif.");
     })
   }
 })
@@ -224,14 +235,14 @@ chrome.runtime.onConnect.addListener(function (port) {
 function checkUserLogin() {
   chrome.storage.sync.get(['userIsLogin'], function (data) {
     if (!data.userIsLogin) {
-      window.location.replace('./login.html')
+      window.location.replace('./login.html');
     }
   })
 }
 
 function getCurrentUser() {
   chrome.storage.local.get(['currentUser'], function (data) {
-    return data.currentUser
+    return data.currentUser;
   })
 }
 
@@ -242,31 +253,31 @@ function response () {
     elements[i].innerHTML = elements[i].innerHTML.replace(
       /\S*@\S*/g,
       '<mark>$&</mark>'
-    )
+    );
   }
 }
 
 //fonction pour récupérer les commentaires du serveur firestore
 const fetchComments = async episodeTitle => {
   let comments = []
-  const querySnapshot = await getDocs(collection(db, episodeTitle))
+  const querySnapshot = await getDocs(collection(db, episodeTitle));
   querySnapshot.forEach(doc => {
     // console.log(doc.id, ' => ', doc.data());
-    comments.push(doc.data())
+    comments.push(doc.data());
   })
   return comments
 }
 
 function updateLecture (playingState) {
-  let button = document.getElementById('play_pause')
+  let button = document.getElementById('play_pause');
   switch (playingState) {
     case 'Lecture':
       currentData.podcastIsPlaying = false
-      button.setAttribute('class', 'play_pause myButton')
+      button.setAttribute('class', 'play_pause myButton');
       break
     case 'Pause':
       currentData.podcastIsPlaying = true
-      button.setAttribute('class', 'pause_play myButton')
+      button.setAttribute('class', 'pause_play myButton');
       break
     default:
       break
@@ -274,78 +285,82 @@ function updateLecture (playingState) {
 }
 
 function updatePopupPlayer (episodeTitle, startingTime, endingTime) {
-  let myEpisodeTitleElement = document.getElementsByClassName('episodeTitle')[0]
-  let myTimecodeElement = document.getElementsByClassName('current_timecode')[0]
-  myEpisodeTitleElement.innerHTML = episodeTitle
-  myTimecodeElement.innerHTML = startingTime + ' / ' + endingTime
+  let myEpisodeTitleElement = document.getElementsByClassName('episodeTitle')[0];
+  let myTimecodeElement = document.getElementsByClassName('current_timecode')[0];
+  myEpisodeTitleElement.innerHTML = episodeTitle;
+  myTimecodeElement.innerHTML = startingTime + ' / ' + endingTime;
 }
 
 const updateStyleForEmptyComment = textArea => {
-  textArea.placeholder = 'Entrez un commentaire valide'
-  textArea.style.setProperty('color', 'white', 'important')
-  textArea.style.setProperty('background-color', 'pink', 'important')
+  textArea.placeholder = 'Entrez un commentaire valide';
+  textArea.style.setProperty('color', 'white', 'important');
+  textArea.style.setProperty('background-color', 'pink', 'important');
 }
 
 const cleanBadWords = comment => {
-  var badWords = ['badword']
-  var sentence = comment
+  var badWords = ['badword'];
+  var sentence = comment;
   for (let i = 0; i < badWords.length; i++) {
-    var regex = new RegExp(badWords[i], 'gi')
-    sentence = sentence.replace(regex, '***')
+    var regex = new RegExp(badWords[i], 'gi');
+    sentence = sentence.replace(regex, '***');
   }
-  return sentence
+  return sentence;
 }
 
 const addDocFirestore = async (collectionName, data) => {
   if (collectionName !== '' && data !== null) {
     addDoc(collection(db, collectionName), data)
       .then(result => {
-        console.log('Document written with ID: ', result.id)
+        console.log('Document written with ID: ', result.id);
       })
       .catch(err => {
-        console.error('Error adding document: ', err)
+        console.error('Error adding document: ', err);
       })
   }
 }
 
 function getPlayPauseOrder () {
-  let button = document.getElementById('play_pause')
-  let buttonClass = button.className
+  let button = document.getElementById('play_pause');
+  let buttonClass = button.className;
 
   if (buttonClass === 'play_pause btn') {
-    return 'play'
+    return 'play';
   } else {
-    return 'pause'
+    return 'pause';
   }
 }
 
 function switchPlayPauseButton () {
-  let button = document.getElementById('play_pause')
-  let buttonClass = button.className
+  let button = document.getElementById('play_pause');
+  let buttonClass = button.className;
   if (buttonClass === 'play_pause btn') {
-    button.className = 'pause_play btn'
+    button.className = 'pause_play btn';
   } else {
-    button.className = 'play_pause btn'
+    button.className = 'play_pause btn';
   }
 }
 
 // fonction d'affichage des comentaires
-const getComments = async timecode => {
-  const querySnapshot = await getDocs(collection(db, episodeTitle)) // récupération depuis firestore
-  querySnapshot.forEach(doc => {
-    if (doc.data().TimeCode == timecode) {
-      var preced
-      var mess = doc.data()
+function getComments(currentCommentt){
+    while(1) {
+      var preced;
+      currentCommentt.forEach(function(messages){
+        mess = messages.data()
+        console.log(mess.TimeCode)
+      
+      if(mess.TimeCode == currentData.timecode.startingTime){
 
-      const messageElement = document.createElement('div')
-      messageElement.id = numMess
+        console.log(mess.TimeCode)
 
-      var pri = 'Private'
+      const messageElement = document.createElement('div');
+      messageElement.id = numMess;
+
+      var pri = 'Private';
       // console.log(mess.UUID)
       if (mess.UUID == userid) {
-        numMess++
+        numMess++;
         if (mess.Private == 0) {
-          pri = 'Public'
+          pri = 'Public';
         }
         messageElement.innerHTML = `<div class="chat-message user-message">
             <div class="chat-message-content">
@@ -367,12 +382,12 @@ const getComments = async timecode => {
             </div>
           </div>
           
-          `
-        const messagesContainer = document.querySelector('#messages')
-        messagesContainer.appendChild(messageElement)
-        preced = messagesContainer
+          `;
+        const messagesContainer = document.querySelector('#messages');
+        messagesContainer.appendChild(messageElement);
+        preced = messagesContainer;
       } else if (mess.Private == 0) {
-        numMess++
+        numMess++;
         messageElement.innerHTML = ` <div class="chat-message">
             <div class="chat-message-content">
             
@@ -392,36 +407,37 @@ const getComments = async timecode => {
             </div>
             </div>
           </div>
-           `
-        const messagesContainer = document.querySelector('#messages')
-        messagesContainer.appendChild(messageElement)
-        preced = messagesContainer
+           `;
+        const messagesContainer = document.querySelector('#messages');
+        messagesContainer.appendChild(messageElement);
+        preced = messagesContainer;
       }
+    }
 
-      messageElement.scrollIntoView({ behavior: 'smooth', block: 'end' })
+      messageElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
 
-      response()
+      response();
 
       if (numMess > limite) {
         //limiter le nombre de messages dans le chat
-        const messagesToDelete = numMess - limite - 1
+        const messagesToDelete = numMess - limite - 1;
 
-        let myDiv = document.getElementById(messagesToDelete)
+        let myDiv = document.getElementById(messagesToDelete);
         // console.log(myDiv)
-        myDiv.parentNode.removeChild(myDiv)
+        myDiv.parentNode.removeChild(myDiv);
       }
-    }
-  })
+    })
+  }
 }
 
 function testAff (mess) {
   //juste une fonction d'injection de commentaire qu'il faut que je renomme
 
-  messageElement = document.createElement('div')
+  messageElement = document.createElement('div');
 
-  var pri = 'Private'
+  var pri = 'Private';
   if (mess.private == 0) {
-    pri = 'Public'
+    pri = 'Public';
   }
   messageElement.innerHTML = `<div class="chat-message user-message">
   
@@ -443,22 +459,22 @@ function testAff (mess) {
 </div>
   </div>
   </div>
-  `
-  numMess++
-  const messagesContainer = document.querySelector('#messages')
-  messagesContainer.appendChild(messageElement)
-  preced = messagesContainer
+  `;
+  numMess++;
+  const messagesContainer = document.querySelector('#messages');
+  messagesContainer.appendChild(messageElement);
+  preced = messagesContainer;
 
   if (numMess > limite) {
     //limiter le nombre de messages dans le chat
-    const messagesToDelete = numMess - limite - 1
+    const messagesToDelete = numMess - limite - 1;
 
-    let myDiv = document.getElementById(messagesToDelete)
+    let myDiv = document.getElementById(messagesToDelete);
     // console.log(myDiv)
-    myDiv.parentNode.removeChild(myDiv)
+    myDiv.parentNode.removeChild(myDiv);
   }
 
-  response()
+  response();
 }
 
 // =========== IDEAS ===========
@@ -466,3 +482,4 @@ function testAff (mess) {
 TODO : créer une fonction qui prends un commentaire en paramètre et qui l'affiche
 TODO : créer une fonction qui prends une liste de commentaire en paramètre et qui les affiche dans le bonne ordre
 */
+
